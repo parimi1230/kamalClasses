4a2538d14599db1c736ab27c03134cb1e90ff1a4
pradeepq <parimi1230@gmail.com>
2019-08-15 00:51:39 +0530

    Commit branch 1

--- src/main/java/com/sample/wrapper/exceptionHandling/DivideByZeroException.java
+++ src/main/java/com/sample/wrapper/exceptionHandling/DivideByZeroException.java
@@ -2,6 +2,7 @@ package com.sample.wrapper.exceptionHandling;
 
 public class DivideByZeroException extends Exception{
 //exceptions are Built here
+//Commit in Branch - 1
 //Mater Commit 1
 //Master Commit 2
 //Master Commit 3
